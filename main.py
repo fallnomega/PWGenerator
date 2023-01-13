d42efa3887d0db96485aa4b00a4f11aef09bd003
fallnomega <fallnomega@gmail.com>
2023-01-13 10:29:59 -0800

    code implemented.

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import random
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print("Welcome to the PyPassword Generator!")
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+letter_total = input("how many letters would you like in your password?\n")
+symbols_total = input("how many symbols would you like in your password?\n")
+numbers_total = input("how many numbers would you like in your password?\n")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+total_char = int(letter_total)+int(symbols_total)+int(numbers_total)
+total_letters = 0
+total_numbers = 0
+total_symbols = 0
+password = []
+restart = True
+while restart:
+  for x in range(total_char):
+    char_option = random.randint(0,2)
+    if char_option ==0 and total_letters != int(letter_total):
+      password.append(random.choice(letters))
+      total_letters+=1
+    elif char_option==1 and total_numbers != int(numbers_total):
+      password.append(random.choice(numbers))
+      total_numbers+=1
+    elif char_option ==2 and total_symbols != int(symbols_total):
+      password.append(random.choice(symbols))
+      total_symbols+=1
+    if len(password) == total_char:
+      restart = False
+new_password=''
+print (f"Your new password is: {new_password.join(password)}")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print(f"char len = {total_char}")
+#
+# print(f"num total = {total_numbers}")
+#
+# print(f"sym total = {total_symbols}")
+#
+# print(f"let total = {total_letters}")
